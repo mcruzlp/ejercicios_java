d55239816cdff008523ecfa994edbfcf2d23e33a
Cruz López Pérez <mcruzlp@gmail.com>
2021-01-19 20:04:13 +0100

    corregido

--- T6/Ej00_primos.java
+++ T6/Ej00_primos.java
@@ -14,20 +14,21 @@ public class Ej00_primos {
   public static void main(String[] args) {
 
     int numPrimos = 0;
+    boolean esPrimo;
 
     do {
       int num = (int) (Math.random() * 999) + 2;
-      //boolean esPrimo = false;
+      esPrimo = true;
       for (int i = 2; i < num; i++) {
-        if (num % i == 0) {
-          //esPrimo = false;
-        } else {
-          //esPrimo = true;
+        if ((num % i) == 0) {
+          esPrimo = false;
+        }
+        if(esPrimo){
           System.out.println(num);
           numPrimos++;
         }
       }
-    }while (numPrimos <= 20);
+    }while (numPrimos < 20);
     System.out.println(numPrimos);
   }
 }
