39434517c6dfc1d5055aefe88ba745d4d39215aa
Cruz López Pérez <mcruzlp@gmail.com>
2021-01-27 11:33:20 +0100

    Update Ej2_esPrimo.java

--- T8-Funciones/Ej2_esPrimo.java
+++ T8-Funciones/Ej2_esPrimo.java
@@ -1,23 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicios 1-14
+ * Crea una biblioteca de funciones matemáticas que contenga las siguientes
+ * funciones. Recuerda que puedes usar unas dentro de otras si es necesario.
+ * Observa bien lo que hace cada función ya que, si las implementas en el orden
+ * adecuado, te puedes ahorrar mucho trabajo. Por ejemplo, la función esCapicua
+ * resulta trivial teniendo voltea y la función siguientePrimo también es muy fácil de
+ * implementar teniendo esPrimo.
+
+ * Ejercicio 2. esPrimo: Devuelve verdadero si el número que se pasa como parámetro
+ * es primo y falso en caso contrario.
+ *
+ * @author cruz
+ *
  */
 package t8_funciones;
 
 import java.util.Scanner;
 import static misfunciones.Matemáticas.esPrimo;
 
-
-/**
- *
- * @author cruz
- */
 public class Ej2_esPrimo {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
     System.out.println("Introduzca el número que desea comprobar si es primo: ");
     
