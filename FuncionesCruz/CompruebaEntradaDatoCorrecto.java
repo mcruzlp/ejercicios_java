ae66bfbe4f4f6bfedb60d2e681b4bc6b012bbe63
Cruz López Pérez <mcruzlp@gmail.com>
2021-02-07 23:24:23 +0100

    update&new

--- FuncionesCruz/CompruebaEntradaDatoCorrecto.java
+++ FuncionesCruz/CompruebaEntradaDatoCorrecto.java
@@ -5,16 +5,22 @@
 package misfunciones;
 
 import java.util.Scanner;
+
 public class CompruebaEntradaDatoCorrecto {
 
-
+  public static void main(String[] args) {
+    
     boolean check;
     long numIntroducido;
+
     Scanner s = new Scanner(System.in);
+
     do {
       check = true;
+
       System.out.print("Introduzca un número par: ");
       numIntroducido = Long.parseLong(s.nextLine());
+
       if (numIntroducido % 2 != 0) { //condición que hay que comprobar
         System.out.println("Lo siento. El número no es par.");
         System.out.println("Vuelva a intentarlo.");
--- FuncionesCruz/MyMaths.java
+++ FuncionesCruz/MyMaths.java
@@ -27,13 +27,13 @@ public class MyMaths {
   }
 
   /**
-   * ****** SIGUIENTE PRIMO ****** Esta función se usa después de haber comprobado
-   * con la función esPrimo si un número es primo o no, y busca el siguiente
-   * número entero primo mayor al número introducido por el usuario.
+   * ****** SIGUIENTE PRIMO ****** Esta función se usa después de haber
+   * comprobado con la función esPrimo si un número es primo o no, y busca el
+   * siguiente número entero primo mayor al número introducido por el usuario.
    *
    * @param x número que se quiere comprobar si es primo
    * @return Devuelve el menor primo que es mayor al número que se pasa como
-   *         parámetro.
+   * parámetro.
    */
   public static int siguientePrimo(int x) {
 
@@ -46,13 +46,23 @@ public class MyMaths {
 
   /**
    * ****** VOLTEA ******
-   * 
+   *
    * @param num número que se quiere dar la vuelta
    * @return devuelve el número volteado
    */
-
   public static int voltea(int num) {
     int numVolteado = 0;
+
+    while (num > 0) {
+      numVolteado = (numVolteado * 10) + (num % 10);
+      num /= 10;
+    }
+
+    return numVolteado;
+  }
+  
+  public static long voltea(long num) {
+    long numVolteado = 0;
 
     while (num > 0) {
       numVolteado = (numVolteado * 10) + (num % 10);
@@ -64,11 +74,10 @@ public class MyMaths {
 
   /**
    * ****** ES CAPICUA ******
-   * 
+   *
    * @param num número que se quiere comprobar si es capicua
    * @return devuelve si el número es capicua o no
    */
-
   public static boolean esCapicua(int num) {
 
     int numVolteado = voltea(num);
@@ -78,24 +87,10 @@ public class MyMaths {
   }
 
   /**
-   * ****** ES PAR ******
-   *
-   * @param x número que se quiere comprobar si es par
-   * @return devuelve si el número es par o no
-   */
-  public static boolean esPar(int x) {
-
-    if (x % 2 != 0) {
-      return false;
-    }
-    return true;
-  }
-
-  /**
    * ****** POTENCIA ******
    *
    * @param base número que hay que multiplicar por sí mismo.
-   * @param exp  número de veces que hay que multiplicarlo.
+   * @param exp número de veces que hay que multiplicarlo.
    * @return devuelve la potencia calculada.
    */
   public static double potencia(double base, double exp) {
@@ -122,6 +117,16 @@ public class MyMaths {
     return numDigitos;
   }
 
+  public static int cuentaDigitos(int num) {
+    int numDigitos = 1;
+
+    while (num > 10) {
+      num /= 10;
+      numDigitos++;
+    }
+    return numDigitos;
+  }
+
   /**
    * ****** CONVIERTE A ARRAY ******
    *
@@ -130,108 +135,79 @@ public class MyMaths {
    */
   public static int[] convierteArray(long num) {
     long numRecortado = num;
-    int numDigitos = 1;
-
-    // número de dígitos.
-    while (numRecortado > 10) {
-      numRecortado /= 10;
-      numDigitos++;
-    }
+    int numDigitos = cuentaDigitos(numRecortado);
 
     int[] numArray = new int[numDigitos];
 
     for (int i = 0; i < numDigitos; i++) {
-      // numArray[i] = num % 10;
+      numArray[i] = (int) (num % 10);
     }
 
     return numArray;
-
   }
 
   /**
    * ****** DEVUELVE EL DÍGITO DE UNA POSICIÓN ******
    *
-   * @param num      número del que se consulta una posición contreta
+   * @param num número del que se consulta una posición contreta
    * @param posicion posición que desea consultares
    * @return devuelve el número que hay en la posición consultada
    */
   public static int posicionDigito(long num, int posicion) {
-    long numRecortado = num;
-    long primerDigito;
-    int numDigitos = 1;
-    int numDivisiones;
-
-    do {
-      // valor del primer dígito y número de dígitos.
-      while (numRecortado > 10) {
-        numRecortado /= 10;
-        numDigitos++;
-      }
-      numDivisiones = numDigitos;
-      primerDigito = numRecortado;
-
-      // int potencia = Math.pow(10,numDivisiones);
-      int potencia = 1;
-      for (int i = 0; i < numDivisiones - 1; i++) {
-        potencia = potencia * 10;
-      }
-
-      numRecortado -= primerDigito * potencia;
-      numDigitos--;
-    } while (numDigitos != 0);
-
-    return posicion;
+    num = voltea(num);
+    while (posicion-- > 0) {
+      num /= 10;
+    }
+    return (int) num % 10;
   }
 
-  /*********
-   * ENCUENTRA EL DÍGITO **************
-   * 
+  /**
+   * ******* ENCUENTRA EL DÍGITO **************
    *
-   * @param num    número del que se consulta una posición contreta
+   *
+   * @param num número del que se consulta una posición contreta
    * @param digito dígito del que se quiere obtener la posición
    * @return devuelve la posición del número consultado
    */
-
   public static int encuentraDigito(int num, int digito) {
 
-    /*
-     * esta función debe encontrar un dígito dentro de un número (ej 08) para ello
-     * quiero usar la función cuentadigito, añadiéndole que guarde el valor si
-     * coincide con el número introducido
-     */
-
-    return 0;
+    int numDigitos = cuentaDigitos(num);
+  
+    for (int i = 0; i <= numDigitos; i++){
+      int numPerdido = posicionDigito(num,i);
+      if (num == numPerdido){
+        return i;
+      } else return -1;
+    }
   }
 
-  /*********
-   * PASA DE BINARIO A DECIMAL **************
-   * 
+  /**
+   * ******* PASA DE BINARIO A DECIMAL **************
+   *
    *
    * @param x número binario que quiere pasarse a decimal
    * @return devuelve el número en decimal
    */
-
-  // public static long binarioDecimal (long x){
-  //
-  // int numDivisiones = 0;
-  //
-  // while (x >= 2){
-  // long resto = x % 2;
-  // x /= 2;
-  // resto += resto*potencia(10,numDivisiones);
-  // numDivisiones++;
-  // } /*terminar de arreglar esto porque cuando el resto es cero, al
-  // multiplicarlo por 100, o 1000 no se va a poner en la posición de las
-  // centenas o millares, igual mejor con array*/
-  //
-  // int num = 0;
-  //
-  // return num;
-  // }
-
-  /*********
-   * CALCULA MEDIA **************
-   * 
+//  public static long binarioDecimal (long x){
+//    
+//    int numDivisiones = 0;
+//    
+//    while (x >= 2){
+//      long resto = x % 2; 
+//      x /= 2;
+//      resto += resto*potencia(10,numDivisiones);
+//      numDivisiones++;
+//    } /*terminar de arreglar esto porque cuando el resto es cero, al
+//    multiplicarlo por 100, o 1000 no se va a poner en la posición de las 
+//    centenas o millares, igual mejor con array*/
+//    
+//    int num = 0;
+//  
+//    return num;
+//  }
+  /**
+   * ******* CALCULA MEDIA **************
+   *
    *
    * @param notas datos a los que calcularle la media
    * @return devuelve la media del total
@@ -243,18 +219,6 @@ public class MyMaths {
       suma += n;
     }
     return suma / notas.length;
-  }
-
-  /*********
-   * ALEATORIO EN UN INTERVALO **************
-   * 
-   * @param min valor más pequeño del intervalo
-   * @param max valor más grande del intervalo
-   * @return un número aleatorio
-   */
-  public static int aleatorio(int min, int max) {
-
-    return (int) (Math.random() * (max - min + 1)) + min;
   }
 
 }
