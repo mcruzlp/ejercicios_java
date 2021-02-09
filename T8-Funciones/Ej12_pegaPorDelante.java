7472910f6438dc3c658b97ee4d16310f978f31cb
Cruz López Pérez <mcruzlp@gmail.com>
2021-02-09 21:21:02 +0100

    update&new

---
+++ T8-Funciones/Ej12_pegaPorDelante.java
@@ -0,0 +1,34 @@
+/** Ejercicios 1-14
+ * Crea una biblioteca de funciones matemáticas que contenga las siguientes
+ * funciones. Recuerda que puedes usar unas dentro de otras si es necesario.
+ * Observa bien lo que hace cada función ya que, si las implementas en el orden
+ * adecuado, te puedes ahorrar mucho trabajo. Por ejemplo, la función esCapicua
+ * resulta trivial teniendo voltea y la función siguientePrimo también es muy
+ * fácil de implementar teniendo esPrimo.
+ * 
+ * Ejercicio 12. pegaPorDelante: Añade un dígito a un número por delante.
+ * 
+ * @author cruz
+ * 
+ */
+package javaapplication24;
+
+import java.util.Scanner;
+import misfunciones.MyMaths;
+
+public class Ej12_pegaPorDelante {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduzca un número: ");
+    int numIntroducido = Integer.parseInt(s.nextLine());
+    
+    System.out.print("Introduzca el dígito que quiere añadirle al número: ");
+    int digitoPegado = Integer.parseInt(s.nextLine());
+    
+    System.out.println("El número final es "
+      + MyMaths.pegaPorDelante(numIntroducido,digitoPegado));
+  }
+  
+}
---
+++ T8-Funciones/Ej13_trozoDeNumero.java
@@ -0,0 +1,32 @@
+/** Ejercicios 1-14
+ * Crea una biblioteca de funciones matemáticas que contenga las siguientes
+ * funciones. Recuerda que puedes usar unas dentro de otras si es necesario.
+ * Observa bien lo que hace cada función ya que, si las implementas en el orden
+ * adecuado, te puedes ahorrar mucho trabajo. Por ejemplo, la función esCapicua
+ * resulta trivial teniendo voltea y la función siguientePrimo también es muy
+ * fácil de implementar teniendo esPrimo.
+ * 
+ * Ejercicio 13. trozoDeNumero: Toma como parámetros las posiciones inicial y
+ * final dentro de un número y devuelve el trozo correspondiente.
+ * 
+ * @author cruz
+ * 
+ */
+package javaapplication24;
+
+import java.util.Scanner;
+import misfunciones.MyMaths;
+
+public class Ej13_trozoDeNumero {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduzca un número: ");
+    int numIntroducido = Integer.parseInt(s.nextLine());
+    
+    System.out.println("El número recortado es " 
+      + MyMaths.trozoDeNumero(numIntroducido));
+  }
+  
+}
