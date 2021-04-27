a8854cd1ebe20bd06b968a592f7f8cad6b22b8ed
Cruz López Pérez <mcruzlp@gmail.com>
2021-04-27 22:51:17 +0200

    new

---
+++ T9-POO/Ej02_Vehiculos/Bicicleta.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * @author Cruz
+ */
+package Ej02_Vehiculo;
+
+public class Bicicleta extends Vehiculo{
+  
+  private String tipoBici;
+  private int talla;
+  private int diametroRueda;
+  private int numPlatos;
+
+  public Bicicleta(String tipoBici, int talla, int diametroRueda, int numPlatos,
+    int kilometrosRecorridos) {
+    
+    super(kilometrosRecorridos);
+    this.tipoBici = tipoBici;
+    this.talla = talla;
+    this.diametroRueda = diametroRueda;
+    this.numPlatos = numPlatos;
+  }
+  
+  public void pedalea() {
+    this.kilometrosRecorridos = (int)(Math.random()*10);
+    System.out.println("\033[33m La bicicleta ha recorrido " +
+      this.kilometrosRecorridos + " km en este tramo");
+    this.setKilometrosRecorridos(this.kilometrosRecorridos);
+    Vehiculo.setKilometrosTotales(Vehiculo.getKilometrosTotales() +
+      this.kilometrosRecorridos);
+  }
+}
---
+++ T9-POO/Ej02_Vehiculos/Coche_bis.java
@@ -0,0 +1,74 @@
+package Ej02_Vehiculos;
+
+/**
+ *
+ * @author cruz
+ */
+public class Coche {
+  
+  // atributos de clase *************************************
+  private static int numCoches = 0;
+  private static int kmTotales = 0;
+  
+  // atributos de instancia *********************************
+  private final String numBastidor = "";
+  private String matricula ="";
+  private String marca ="";
+  private String modelo ="";
+  private int kilometraje = 0;
+
+  // constructor *********************************************
+  public Coche() {
+    Coche.numCoches++;
+  }
+  
+  public Coche(String matricula) {
+    this.matricula = matricula;
+    Coche.numCoches++;
+  }
+  
+  public Coche(String matricula, String marca, String modelo) {
+    this.matricula = matricula;
+    this.marca = marca;
+    this.modelo = modelo;
+    Coche.numCoches++;
+  }
+  
+  // getter y setter ******************************************
+  public int getKilometraje() {
+    return this.kilometraje;
+  }
+
+  public void setKilometraje(int km) {
+    this.kilometraje = km;
+  }
+
+  public static int getKmTotal() {
+    return Coche.kmTotales;
+  }
+  
+  
+  
+  // métodos *********************************************
+  public void recorre(int distancia){
+    this.kilometraje += distancia;
+    kmTotales += distancia;
+  }
+  
+  /**
+   *
+   * @return
+   */
+  @Override
+  public String toString() {
+    String resultado = "======================\n";
+    resultado += "Matrícula: " + this.matricula;
+    resultado += "\nMarca: " + this.marca;
+    resultado += "\nModelo: " + this.modelo;
+    resultado += "\nKilómetros recorridos: " + this.kilometraje;
+    resultado += "\n======================\n";
+    
+    return resultado;
+  }
+  
+}
